18eae75b22d3d89ca52ad292eb6e42e78af29706
RasmusBrostroem <201706904@post.au.dk>
2024-02-13 14:33:22 +0100

    Create diffusion process.py
    Created simulation of the Itô diffusion process using the Euler Maruyma Method

---
+++ diffusion process.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Callable
+
+# Define the drift function and diffusion coefficient
+def b(x: float, t: float) -> float:
+    # return the drift function evaluated at x and t
+    return -x/2
+
+def sigma(x: float, t: float) -> float:
+    # return the diffusion coefficient evaluated at x and t
+    return 1
+
+def EulerMaruymaMethod(b: Callable[[float, float], float],
+                       sigma: Callable[[float, float], float],
+                       T: float,
+                       dt: float,
+                       x0: float):
+    N = int(T/dt) # number of time steps
+    x = np.zeros(N+1)
+    t = np.zeros(N+1)
+    x[0] = x0
+    t[0] = 0.0
+
+    for n in range(N):
+        t[n+1] = t[n] + dt
+        z = np.random.normal(loc=0.0, scale=np.sqrt(dt))
+        x[n+1] = x[n] + b(x[n], t[n])*dt + sigma(x[n], t[n])*z
+
+    return x, t
+
+x, t = EulerMaruymaMethod(b, sigma, 100, 0.01, 0)
+
+# Plot the simulated values
+plt.plot(t, x)
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.show()
