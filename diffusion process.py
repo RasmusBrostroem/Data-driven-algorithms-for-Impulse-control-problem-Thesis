5ee014349a956b611a1d02232b2d0b9e220bdcef
RasmusBrostroem <201706904@post.au.dk>
2024-02-13 14:38:15 +0100

    Added docstring to simulation function

--- diffusion process.py
+++ diffusion process.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import Callable
+from typing import Callable, Union
 
 # Define the drift function and diffusion coefficient
 def b(x: float, t: float) -> float:
@@ -15,7 +15,22 @@ def EulerMaruymaMethod(b: Callable[[float, float], float],
                        sigma: Callable[[float, float], float],
                        T: float,
                        dt: float,
-                       x0: float):
+                       x0: float) -> Union[np.array, np.array]:
+    """Simmulating the Itô diffusion process using Euler Maruyma Method
+    See https://en.wikipedia.org/wiki/Euler%E2%80%93Maruyama_method
+
+    Args:
+        b (Callable[[float, float], float]): The drift function
+        sigma (Callable[[float, float], float]): The diffusion coefficient
+        T (float): Number of time the process should run
+        dt (float): The increment of time before simulating new value
+        x0 (float): Initial value of process
+
+    Returns:
+        Union[np.array, np.array]: 
+            x-values: the simulated values of the process
+            t-values: the time steps, where simulated values were generated
+    """
     N = int(T/dt) # number of time steps
     x = np.zeros(N+1)
     t = np.zeros(N+1)
