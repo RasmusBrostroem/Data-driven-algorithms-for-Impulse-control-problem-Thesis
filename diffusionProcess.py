54748ea436db813cc2481ce67a56797c8c9e65bd
RasmusBrostroem <201706904@post.au.dk>
2024-02-23 21:59:17 +0100

    Made classes and tested strategy

--- diffusionProcess.py
+++ diffusionProcess.py
@@ -17,61 +17,8 @@ def sigma(x: float, t: float) -> float:
     # return the diffusion coefficient evaluated at x and t
     return 1
 
-def EulerMaruymaMethod(b: Callable[[float, float], float],
-                       sigma: Callable[[float, float], float],
-                       T: float,
-                       dt: float,
-                       x0: float) -> Union[np.array, np.array]:
-    """Simmulating the Itô diffusion process using Euler Maruyma Method
-    See https://en.wikipedia.org/wiki/Euler%E2%80%93Maruyama_method
-
-    Args:
-        b (Callable[[float, float], float]): The drift function
-        sigma (Callable[[float, float], float]): The diffusion coefficient
-        T (float): Number of time the process should run
-        dt (float): The increment of time before simulating new value
-        x0 (float): Initial value of process
-
-    Returns:
-        Union[np.array, np.array]: 
-            x-values: the simulated values of the process
-            t-values: the time steps, where simulated values were generated
-    """
-    N = int(T/dt) # number of time steps
-    x = np.zeros(N+1)
-    t = np.zeros(N+1)
-    x[0] = x0
-    t[0] = 0.0
-
-    for n in range(N):
-        t[n+1] = t[n] + dt
-        z = np.random.normal(loc=0.0, scale=np.sqrt(dt))
-        x[n+1] = x[n] + b(x[n], t[n])*dt + sigma(x[n], t[n])*z
-
-    return x, t
-
-
-def diffusionProcessStep(b: Callable[[float, float], float],
-                         sigma: Callable[[float, float], float],
-                         x: float,
-                         t: float,
-                         dt: float) -> float:
-    """_summary_
-
-    Args:
-        b (Callable[[float, float], float]): _description_
-        sigma (Callable[[float, float], float]): _description_
-        x (float): _description_
-        t (float): _description_
-        dt (float): _description_
-
-    Returns:
-        float: _description_
-    """
-    return x + b(x, t)*dt + sigma(x, t)*np.random.normal(loc=0.0, scale=np.sqrt(dt))
-
 def reward(x):
-    return 1 - np.abs(1-x)**2
+    return 10 - np.abs(4-3*x)**2
 
 def get_y1_and_zeta(g):
     roots = fsolve(g, [0, 10000])
@@ -84,40 +31,116 @@ def get_y1_and_zeta(g):
 
     return y1, zeta
 
-def getC_b_sigma(b, sigma):
-    def inner_integral(u, b, sigma):
-        f = lambda y: 2*b(y, 0)/sigma(y, 0)**2
-        return quad(f, 0, u)[0]
-    
-    f = lambda u: 1/sigma(u,0)**2 * np.exp(inner_integral(u, b, sigma))
-    return quad(f, -np.inf, np.inf)[0]
+class DiffusionProcess():
+    def __init__(self, b, sigma) -> None:
+        self.b = b
+        self.sigma = sigma
 
-def invariant_density(x, b, sigma):
-    def integral(x, b, sigma):
-        f = lambda y: 2*b(y, 0)/sigma(y, 0)**2
-        if isinstance(x, Iterable):
-            return np.array(list(map(partial(quad, f, 0), x)))[:, 0]
+
+    def EulerMaruymaMethod(self,
+                           T: float,
+                           dt: float,
+                           x0: float) -> Union[np.array, np.array]:
+        """Simmulating the Itô diffusion process using Euler Maruyma Method
+        See https://en.wikipedia.org/wiki/Euler%E2%80%93Maruyama_method
+
+        Args:
+            b (Callable[[float, float], float]): The drift function
+            sigma (Callable[[float, float], float]): The diffusion coefficient
+            T (float): Number of time the process should run
+            dt (float): The increment of time before simulating new value
+            x0 (float): Initial value of process
+
+        Returns:
+            Union[np.array, np.array]: 
+                x-values: the simulated values of the process
+                t-values: the time steps, where simulated values were generated
+        """
+        N = int(T/dt) # number of time steps
+        x = np.zeros(N+1)
+        t = np.zeros(N+1)
+        x[0] = x0
+        t[0] = 0.0
+
+        for n in range(N):
+            t[n+1] = t[n] + dt
+            z = np.random.normal(loc=0.0, scale=np.sqrt(dt))
+            x[n+1] = x[n] + self.b(x[n], t[n])*dt + self.sigma(x[n], t[n])*z
+
+        return x, t
+
+    def step(self,
+             x: float,
+             t: float,
+             dt: float) -> float:
+        """_summary_
+
+        Args:
+            b (Callable[[float, float], float]): _description_
+            sigma (Callable[[float, float], float]): _description_
+            x (float): _description_
+            t (float): _description_
+            dt (float): _description_
+
+        Returns:
+            float: _description_
+        """
+        return x + self.b(x, t)*dt + self.sigma(x, t)*np.random.normal(loc=0.0, scale=np.sqrt(dt))
+
+
+    def getC_b_sigma(self):
+        def inner_integral(u):
+            f = lambda y: 2*self.b(y, 0)/self.sigma(y, 0)**2
+            return quad(f, 0, u)[0]
         
-        return quad(f, 0, x)[0]
+        f = lambda u: 1/self.sigma(u,0)**2 * np.exp(inner_integral(u))
+        return quad(f, -np.inf, np.inf)[0]
 
+    def invariant_density(self, x):
+        def integral(x):
+            f = lambda y: 2*self.b(y, 0)/self.sigma(y, 0)**2
+            if isinstance(x, Iterable):
+                return np.array(list(map(partial(quad, f, 0), x)))[:, 0]
+            return quad(f, 0, x)[0]
+
+        C_b_sigma = self.getC_b_sigma()
+        return 1/C_b_sigma * np.exp(integral(x))
+
+    def xi(self, x):
+        def inner_integral(y):
+            return quad(self.invariant_density, -np.inf, y)[0]
+        
+        f = lambda y: 1/(self.sigma(y,0)**2 * self.invariant_density(y)) * inner_integral(y)
+        return 2*quad(f, 0, x)[0]
+
+    def erfi(self, x, N):
+        f = lambda n: x**(2*n+1)/(math.factorial(n)*(2*n+1))
+        return 2/np.sqrt(np.pi) * sum(map(f, range(N)))
+
+    def xi_theoretical(self, x):
+        return float(x**2 * hyp2f2(1,1,3/2, 2, x**2/2) + np.pi * self.erfi(x/np.sqrt(2), 10))
+
+class OptimalStrategy():
+    def __init__(self, diffusionProcess, rewardFunc):
+        self.difPros = diffusionProcess
+        self.g = rewardFunc
+        self.y1, self.zeta = get_y1_and_zeta(rewardFunc)
+        self.y_star = self.get_optimal_threshold()
+        self.reward = 0
+
+    def get_optimal_threshold(self):
+        obj = lambda y: -self.g(y)/self.difPros.xi_theoretical(y)
+        result = minimize_scalar(obj, bounds=(self.y1, self.zeta), method="bounded", options={'xatol': 1e-8})
+        return result.x
     
-    C_b_sigma = getC_b_sigma(b, sigma)
+    def take_decision(self, x):
+        if x >= self.y_star:
+            self.reward += self.g(x)
+            return True
+        
+        return False
 
-    return 1/C_b_sigma * np.exp(integral(x, b, sigma))
 
-def xi(x, b, sigma):
-    def inner_integral(y, b, sigma):
-        return quad(invariant_density, -np.inf, y, args=(b, sigma))[0]
-    
-    f = lambda y: 1/(sigma(y,0)**2 * invariant_density(y, b, sigma)) * inner_integral(y, b, sigma)
-    return 2*quad(f, 0, x)[0]
-
-def erfi(x, N):
-    f = lambda n: x**(2*n+1)/(math.factorial(n)*(2*n+1))
-    return 2/np.sqrt(np.pi) * sum(map(f, range(N)))
-
-def xi_teoretial(x):
-    return float(x**2 * hyp2f2(1,1,3/2, 2, x**2/2) + np.pi * erfi(x/np.sqrt(2), 10))
 
 if __name__ == "__main__":
     # x, t = EulerMaruymaMethod(b, sigma, 100, 0.01, 0)
@@ -126,30 +149,31 @@ if __name__ == "__main__":
     # plt.xlabel('Time')
     # plt.ylabel('Value')
     # plt.show()
-    f = lambda y: -reward(y)/xi_teoretial(y)
+    difPros = DiffusionProcess(b, sigma)
+    optStrat = OptimalStrategy(difPros, reward)
+    print(f"optimal threshold: {optStrat.y_star}")
+    T = 10
+    dt = 0.01
+    t = [0]
+    X_strat = [0]
+    x_plot = []
+    t_plot = []
+    i = 0
+    while t[i] < T:
+        if optStrat.take_decision(x=X_strat[i]):
+            t_plot.append(t)
+            x_plot.append(X_strat)
+            t = [t[i]]
+            X_strat = [0]
+            i = 0
 
-    y1, zeta = get_y1_and_zeta(reward)
-    y = np.linspace(y1+0.00001, zeta, 20)
-    gs = list(map(reward, y))
-    # xif = lambda y: xi(y, b, sigma)
-    # xis = list(map(xif, y))
-    xis_theo = list(map(xi_teoretial, y))
-    print(gs)
-    print(xis_theo)
-    # plt.plot(y, xis, label="Calculated")
-    # plt.show()
-    # plt.plot(y, xis_theo, label="Theoretical")
-    # plt.legend()
-    # plt.show()
+        t.append(t[i] + dt)
+        X_strat.append(difPros.step(X_strat[i], t[i], dt))
+        i += 1
 
-    vals = [g/z for g, z in zip(gs,xis_theo)]
-    result = minimize_scalar(f, bounds=(y1, zeta), method="bounded", options={'xatol': 1e-8})
-    y_star = result.x
-    print(f"Optimal stopping time y: {y_star}")
-    print(f"Optimal reward per expected time unit: {-f(y_star)}")
-    plt.plot(y, gs)
-    plt.show()
-    plt.plot(y, xis_theo)
-    plt.show()
-    plt.plot(y, vals)
+    total_reward = optStrat.reward
+    print(f"Total reward was: {total_reward}")
+    for t, x in zip(t_plot, x_plot):
+        plt.plot(t,x, color="b")
+        
     plt.show()
