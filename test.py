cd8dc8914a2a3bd6f479274b29cff7ee6a95aef2
RasmusBrostroem <201706904@post.au.dk>
2024-03-28 11:43:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -29,36 +29,42 @@ dataStrat.bandwidth = 1/np.sqrt(100)
 
 data, t = diffPros.EulerMaruymaMethod(5000, 0.01, 0)
 
-fitStart = time()
 dataStrat.fit(data)
-fitEnd = time()
-print(f"Original fit took {fitEnd-fitStart} seconds")
-
-start = time()
-estimate, nevals, nitr = dataStrat.estimate_threshold()
-t1 = time()
-estimate_new, nevals_new, nitr_new = dataStrat.estimate_threshold_new()
-t2 = time()
-
-dataStrat.clear_sorted_dict()
-
-fitStart_new = time()
-dataStrat.fit_new(data)
-fitEnd_new = time()
-print(f"new fit took {fitEnd_new-fitStart_new} seconds")
-
-t3 = time()
-estimate_new_new, nevals_new_new, nitr_new_new = dataStrat.estimate_threshold_new_new()
-t4 = time()
-
-print(f"original estimate took {t1-start} whith {nevals} evaluations, which on average is {(t1-start)/nevals}")
-print(f"new estimate took {t2-t1} whith {nevals_new} evaluations, which on average is {(t2-t1)/nevals_new}")
-print(f"new new estimate took {t4-t3} whith {nevals_new_new} evaluations, which on average is {(t4-t3)/nevals_new_new}")
 
 
-print(f"old estimate = {estimate}")
-print(f"new estimate = {estimate_new}")
-print(f"new new estimate = {estimate_new_new}")
+
+
+# fitStart = time()
+# dataStrat.fit(data)
+# fitEnd = time()
+# print(f"Original fit took {fitEnd-fitStart} seconds")
+
+# start = time()
+# estimate, nevals, nitr = dataStrat.estimate_threshold()
+# t1 = time()
+# estimate_new, nevals_new, nitr_new = dataStrat.estimate_threshold_new()
+# t2 = time()
+
+
+# dataStrat.clear_sorted_dict()
+
+# fitStart_new = time()
+# dataStrat.fit_new(data)
+# fitEnd_new = time()
+# print(f"new fit took {fitEnd_new-fitStart_new} seconds")
+
+# t3 = time()
+# estimate_new_new, nevals_new_new, nitr_new_new = dataStrat.estimate_threshold_new_new()
+# t4 = time()
+
+# print(f"original estimate took {t1-start} whith {nevals} evaluations, which on average is {(t1-start)/nevals}")
+# print(f"new estimate took {t2-t1} whith {nevals_new} evaluations, which on average is {(t2-t1)/nevals_new}")
+# print(f"new new estimate took {t4-t3} whith {nevals_new_new} evaluations, which on average is {(t4-t3)/nevals_new_new}")
+
+
+# print(f"old estimate = {estimate}")
+# print(f"new estimate = {estimate_new}")
+# print(f"new new estimate = {estimate_new_new}")
 
 
 
