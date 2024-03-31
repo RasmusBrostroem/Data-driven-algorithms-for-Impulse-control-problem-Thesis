6e3b93155e5f2b9cfbfc3635ef41dbc80acf3ace
RasmusBrostroem <201706904@post.au.dk>
2024-03-31 12:56:07 +0200

    cleaned code after tests

--- strategies.py
+++ strategies.py
@@ -6,24 +6,16 @@ from scipy.integrate import quad
 from scipy.interpolate import interp1d
 from functools import partial
 from collections.abc import Iterable
-import math
 from mpmath import hyp2f2
-from sklearn.neighbors import KernelDensity
-from sklearn.model_selection import GridSearchCV
 from statsmodels.nonparametric.kde import KDEUnivariate
 from statsmodels.distributions.empirical_distribution import ECDF
 from collections.abc import Iterable
-
-from time import time
-
-from functools import lru_cache
-from sortedcontainers import SortedDict
 
 from diffusionProcess import DiffusionProcess
 
 
 def reward(x):
-    return 1/2 - np.abs(1-x)**2
+    return 9/10 - np.abs(1-x)**(5)
     # return 10 - np.abs(4-3*x)**2
 
 def get_y1_and_zeta(g):
@@ -94,7 +86,6 @@ class DataDrivenImpulseControl():
         self.cdf = None
 
         self.xi_evaluated_x = {}
-        self.cdf_evaluated_x = None
         self.pdf_evaluated_x = None
 
     def update_attributes_on_kwargs(self, **kwargs):
@@ -114,7 +105,6 @@ class DataDrivenImpulseControl():
     def clear_cache(self):
         self.xi_evaluated_x.clear()
         self.pdf_evaluated_x = None
-        self.cdf_evaluated_x = None
     
     def kernel_fit(self, data: list[float]) -> None:
         self.kde = KDEUnivariate(data)
@@ -122,27 +112,17 @@ class DataDrivenImpulseControl():
 
     def ecdf_fit(self, data: list[float]) -> None:
         self.cdf = ECDF(data)
-    
+
     def fit(self, data: list[float]) -> None:
         self.clear_cache()
         self.kernel_fit(data)
         self.ecdf_fit(data)
-
-    def fit_new(self, data: list[float]) -> None:
-        self.clear_cache()
-        self.kernel_fit(data)
-        self.ecdf_fit(data)
-        xs = np.linspace(0, self.zeta, 100)
+        xs = np.linspace(0, self.zeta, int(100*self.zeta))
         pdf_values = np.array([self.pdf_eval(x) for x in xs])
         self.pdf_evaluated_x = interp1d(xs, pdf_values, kind="linear")
-        # cdf_values = np.array([self.cdf_eval(x) for x in xs])
-        # self.cdf_evaluated_x = interp1d(xs, cdf_values, kind="linear")
     
     def cdf_eval(self, x: Union[list[float], float]) -> Union[list[float], float]:
         return self.cdf(x)
-    
-    def cdf_eval_interpolate(self, x):
-        return self.cdf_evaluated_x(x)
     
     def pdf_eval(self, x: float) -> float:
         return self.kde.evaluate(x)[0]
@@ -151,39 +131,9 @@ class DataDrivenImpulseControl():
         return self.pdf_evaluated_x(x)
     
     def xi_eval(self, x):
-        f = lambda y: self.cdf_eval(y)/(max(self.pdf_eval(y), self.a)*self.sigma(y)**2)
-        xi_estimate = 2*quad(f, 0, x, limit=250, epsabs=1e-3)[0]
-        return np.maximum(xi_estimate, self.M1)
-
-    def xi_eval_new(self, x):
-        f = lambda y: self.cdf_eval(y)/(max(self.pdf_eval(y), self.a)*self.sigma(y)**2)
-
-        if not self.xi_evaluated_x:
-            #print(f"Found no lower x when evaluating x={x}")
-            xi_estimate = 2*quad(f, 0, x, limit=250, epsabs=1e-2)[0]
-            self.xi_evaluated_x[x] = xi_estimate
-            return np.maximum(xi_estimate, self.M1)
-
-        closest_x_evaluated, xi_val = min(self.xi_evaluated_x.items(), key=lambda y: abs(x - y[0]))
-
-        if closest_x_evaluated <= x:
-            #print(f"Found lower x evaluated = {closest_x_evaluated} with new x to evaluate {x}")
-            xi_estimate = xi_val + 2*quad(f, closest_x_evaluated, x, limit=250, epsabs=1e-2)[0] 
-            
-            self.xi_evaluated_x[x] = xi_estimate
-            return np.maximum(xi_estimate, self.M1)
-        
-        #print(f"Found upper x evaluated = {closest_x_evaluated} with new x to evaluate {x}")
-        xi_estimate = xi_val - 2*quad(f, x, closest_x_evaluated, limit=250, epsabs=1e-2)[0] 
-        
-        self.xi_evaluated_x[x] = xi_estimate
-        return np.maximum(xi_estimate, self.M1)
-    
-    def xi_eval_new_new(self, x):
         f = lambda y: self.cdf_eval(y)/(max(self.pdf_eval_interpolate(y), self.a)*self.sigma(y)**2)
 
         if not self.xi_evaluated_x:
-            #print(f"Found no lower x when evaluating x={x}")
             xi_estimate = 2*quad(f, 0, x, limit=250, epsabs=1e-2)[0]
             self.xi_evaluated_x[x] = xi_estimate
             return np.maximum(xi_estimate, self.M1)
@@ -191,32 +141,20 @@ class DataDrivenImpulseControl():
         closest_x_evaluated, xi_val = min(self.xi_evaluated_x.items(), key=lambda y: abs(x - y[0]))
 
         if closest_x_evaluated <= x:
-            #print(f"Found lower x evaluated = {closest_x_evaluated} with new x to evaluate {x}")
             xi_estimate = xi_val + 2*quad(f, closest_x_evaluated, x, limit=250, epsabs=1e-2)[0] 
             
             self.xi_evaluated_x[x] = xi_estimate
             return np.maximum(xi_estimate, self.M1)
         
-        #print(f"Found upper x evaluated = {closest_x_evaluated} with new x to evaluate {x}")
         xi_estimate = xi_val - 2*quad(f, x, closest_x_evaluated, limit=250, epsabs=1e-2)[0] 
         
         self.xi_evaluated_x[x] = xi_estimate
         return np.maximum(xi_estimate, self.M1)
-
+    
     def estimate_threshold(self) -> float:
         obj = lambda y: -self.g(y)/self.xi_eval(y)
-        result = minimize_scalar(obj, bounds=(self.y1, self.zeta), method="bounded", options={'xatol': 1e-4})
-        return result.x, result.nfev, result.nit
-    
-    def estimate_threshold_new(self) -> float:
-        obj = lambda y: -self.g(y)/self.xi_eval_new(y)
         result = minimize_scalar(obj, bounds=(self.y1, self.zeta), method="bounded", options={'xatol': 1e-2})
-        return result.x, result.nfev, result.nit
-    
-    def estimate_threshold_new_new(self) -> float:
-        obj = lambda y: -self.g(y)/self.xi_eval_new_new(y)
-        result = minimize_scalar(obj, bounds=(self.y1, self.zeta), method="bounded", options={'xatol': 1e-2})
-        return result.x, result.nfev, result.nit
+        return result.x
     
     def simulate(self, diffpros: DiffusionProcess, T: int, dt: float) -> float:
         self.kde = None
@@ -241,7 +179,7 @@ class DataDrivenImpulseControl():
             
             if reachedZeta and X <= 0:
                 self.fit(data)
-                threshold, _, _ = self.estimate_threshold()
+                threshold = self.estimate_threshold()
                 thresholds.append(threshold)
                 exploring = False
                 reachedZeta = False
@@ -254,85 +192,7 @@ class DataDrivenImpulseControl():
             
             X = diffpros.step(X, t, dt)
             t += dt
-        print(f"\n Number of thresholds calculated {len(thresholds)}")
-        return cumulativeReward, S_t
-    
-    def simulate_new(self, diffpros: DiffusionProcess, T: int, dt: float) -> float:
-        self.kde = None
-        self.cdf = None
 
-        data = []
-        X = 0
-        t = 0
-        S_t = 0
-        reachedZeta = False
-        exploring = True
-        threshold = None
-        cumulativeReward = 0
-        thresholds = []
-
-        while t < T:
-            if exploring:
-                data.append(X)
-                S_t += dt
-                if X >= self.zeta:
-                    reachedZeta = True
-            
-            if reachedZeta and X <= 0:
-                self.fit(data)
-                threshold, _, _ = self.estimate_threshold_new()
-                thresholds.append(threshold)
-                exploring = False
-                reachedZeta = False
-            
-            if not exploring and X >= threshold:
-                cumulativeReward += self.g(X)
-                X = 0
-                if S_t < t**(2/3):
-                    exploring = True
-            
-            X = diffpros.step(X, t, dt)
-            t += dt
-        print(f"\n Number of thresholds calculated {len(thresholds)}")
-        return cumulativeReward, S_t
-    
-    def simulate_new_new(self, diffpros: DiffusionProcess, T: int, dt: float) -> float:
-        self.kde = None
-        self.cdf = None
-
-        data = []
-        X = 0
-        t = 0
-        S_t = 0
-        reachedZeta = False
-        exploring = True
-        threshold = None
-        cumulativeReward = 0
-        thresholds = []
-
-        while t < T:
-            if exploring:
-                data.append(X)
-                S_t += dt
-                if X >= self.zeta:
-                    reachedZeta = True
-            
-            if reachedZeta and X <= 0:
-                self.fit_new(data)
-                threshold, _, _ = self.estimate_threshold_new_new()
-                thresholds.append(threshold)
-                exploring = False
-                reachedZeta = False
-            
-            if not exploring and X >= threshold:
-                cumulativeReward += self.g(X)
-                X = 0
-                if S_t < t**(2/3):
-                    exploring = True
-            
-            X = diffpros.step(X, t, dt)
-            t += dt
-        print(f"\n Number of thresholds calculated {len(thresholds)}")
         return cumulativeReward, S_t
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -34,8 +34,7 @@ def time_function(func, args_list, repetitions=10):
     average_time = total_time / repetitions
     return average_time
 
-T = 1000
-
+T = 100
 
 diffPros = DiffusionProcess(b=b, sigma=sigma)
 opStrat = OptimalStrategy(diffusionProcess=diffPros, rewardFunc=reward)
@@ -44,64 +43,19 @@ dataStrat.bandwidth = 1/np.sqrt(T)
 
 data, t = diffPros.EulerMaruymaMethod(T, 0.01, 0)
 
-
-# ogfit = time_function(dataStrat.fit, [data])
-# print(f"Original fit took {ogfit} on average")
-# # ogEstimate = time_function(dataStrat.estimate_threshold, [])
-# # print(f"Original estimate took {ogEstimate} on average")
-# newEstimate = time_function(dataStrat.estimate_threshold_new, [])
-# print(f"new estimate took {newEstimate} on average, which is a total average time of {newEstimate+ogfit}")
-
-# dataStrat.clear_cache()
-# newFit = time_function(dataStrat.fit_new, [data])
-# print(f"New fit took {newFit} on average")
-# newNewEstimate = time_function(dataStrat.estimate_threshold_new_new, [])
-# print(f"new new estimate took {newNewEstimate} on average, which is a total average time of {newNewEstimate+newFit}")
-
-# fitStart = time()
-# dataStrat.fit(data)
-# fitEnd = time()
-# print(f"Original fit took {fitEnd-fitStart} seconds")
+# diffPros.generate_noise(T, 0.01)
 
 # start = time()
-# estimate, nevals, nitr = dataStrat.estimate_threshold()
+# r, S_T = dataStrat.simulate(diffPros, T, 0.01)
 # t1 = time()
-# estimate_new, nevals_new, nitr_new = dataStrat.estimate_threshold_new()
+# r1, S_T1 = dataStrat.simulate_new(diffPros, T, 0.01)
 # t2 = time()
+# r2, S_T2 = dataStrat.simulate_new_new(diffPros, T, 0.01)
+# end = time()
 
-# dataStrat.clear_cache()
-
-# fitStart_new = time()
-# dataStrat.fit_new(data)
-# fitEnd_new = time()
-# print(f"New fit took {fitEnd_new-fitStart_new} seconds")
-
-# t3 = time()
-# estimate_new_new, nevals_new_new, nitr_new_new = dataStrat.estimate_threshold_new_new()
-# t4 = time()
-
-# print(f"original estimate took {t1-start} whith {nevals} evaluations, which on average is {(t1-start)/nevals}")
-# print(f"new estimate took {t2-t1} whith {nevals_new} evaluations, which on average is {(t2-t1)/nevals_new}, but with fitting total time was: {t2-t1 + fitEnd-fitStart}")
-# print(f"new new estimate took {t4-t3} whith {nevals_new_new} evaluations, which on average is {(t4-t3)/nevals_new_new}, but with fitting total time was: {t4-t3 + fitEnd_new-fitStart_new}")
-
-# print(f"Optimal threshold = {opStrat.y_star}")
-# print(f"old estimate = {estimate}")
-# print(f"new estimate = {estimate_new}")
-# print(f"new new estimate = {estimate_new_new}")
-
-diffPros.generate_noise(T, 0.01)
-
-start = time()
-r, S_T = dataStrat.simulate(diffPros, T, 0.01)
-t1 = time()
-r1, S_T1 = dataStrat.simulate_new(diffPros, T, 0.01)
-t2 = time()
-r2, S_T2 = dataStrat.simulate_new_new(diffPros, T, 0.01)
-end = time()
-
-print(f"Original simulation took = {t1-start} with reward {r} and exploration time {S_T}")
-print(f"New simulation took = {t2-t1} with reward {r1} and exploration time {S_T1}")
-print(f"New New simulation took = {end-t2} with reward {r2} and exploration time {S_T2}")
+# print(f"Original simulation took = {t1-start} with reward {r} and exploration time {S_T}")
+# print(f"New simulation took = {t2-t1} with reward {r1} and exploration time {S_T1}")
+# print(f"New New simulation took = {end-t2} with reward {r2} and exploration time {S_T2}")
 
 
 # cProfile.run("dataStrat.estimate_threshold()", sort="cumtime")
