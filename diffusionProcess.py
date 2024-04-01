f44f25afd8edbaf92723394bd1904504778df709
RasmusBrostroem <201706904@post.au.dk>
2024-04-01 22:20:42 +0200

    Update diffusionProcess.py
    Changed limits variable in the quad calls

--- diffusionProcess.py
+++ diffusionProcess.py
@@ -12,8 +12,8 @@ def drift(x: float) -> float:
     return -x/2
 
 def generate_linear_drift(C: float, A: float):
-    #return lambda x: -C*x if x <= 0 else (-C*(x-A) if x > A else 0)
-    return lambda x: 0 if x>=-A and x<=A else -C*(x-np.sign(x)*A)
+    return lambda x: -C*x if x <= 0 else (-C*(x-A) if x > A else 0)
+    #return lambda x: 0 if x>-A and x<A else -C*(x-np.sign(x)*A)
 
 def sigma(x: float) -> float:
     # return the diffusion coefficient evaluated at x and t
@@ -58,7 +58,7 @@ class DiffusionProcess():
         for n in range(N):
             t[n+1] = t[n] + dt
             z = np.random.normal(loc=0.0, scale=np.sqrt(dt))
-            x[n+1] = x[n] + self.b(x[n], t[n])*dt + self.sigma(x[n], t[n])*z
+            x[n+1] = x[n] + self.b(x[n])*dt + self.sigma(x[n])*z
 
         return x, t
 
@@ -80,21 +80,21 @@ class DiffusionProcess():
         def integral(x):
             f = lambda y: 2*self.b(y)/self.sigma(y)**2
             if isinstance(x, Iterable):
-                return np.array(list(map(partial(quad, f, 0, limit=250, epsabs = 1e-3), x)))[:, 0]
-            return quad(f, 0, x, epsabs=1e-3, limit=250)[0]
+                return np.array(list(map(partial(quad, f, 0, limit=100, epsabs = 1e-3), x)))[:, 0]
+            return quad(f, 0, x, epsabs=1e-3, limit=100)[0]
 
         return 1/self.C_b_sigma_val * np.exp(integral(x))
 
     def xi(self, x):
         def inner_integral(y):
             if isinstance(y, Iterable):
-                return np.array(list(map(partial(quad, self.invariant_density, -np.inf, epsabs=1e-3, limit=250), y)))[:, 0]
-            return quad(self.invariant_density, -np.inf, y, epsabs=1e-3, limit=250)[0]
+                return np.array(list(map(partial(quad, self.invariant_density, -np.inf, epsabs=1e-3, limit=100), y)))[:, 0]
+            return quad(self.invariant_density, -np.inf, y, epsabs=1e-3, limit=100)[0]
         
         f = lambda y: 1/(self.sigma(y)**2 * self.invariant_density(y)) * inner_integral(y)
         if isinstance(x, Iterable):
-            return 2*np.array(list(map(partial(quad, f, 0, epsabs=1e-3, limit=250), x)))[:, 0]
-        return 2*quad(f, 0, x, epsabs=1e-3, limit=250)[0]
+            return 2*np.array(list(map(partial(quad, f, 0, epsabs=1e-3, limit=100), x)))[:, 0]
+        return 2*quad(f, 0, x, epsabs=1e-3, limit=100)[0]
 
     def erfi(self, x, N):
         f = lambda n: x**(2*n+1)/(math.factorial(n)*(2*n+1))
